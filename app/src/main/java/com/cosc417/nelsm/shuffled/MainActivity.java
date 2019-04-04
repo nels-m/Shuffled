c52ccd53e46b43e4daf825965b90a5604ac7f7bc
nels <nels.murray@gmail.com>
2019-04-03 17:49:20 -0700

    added dictionary

--- app/src/main/java/com/cosc417/nelsm/shuffled/MainActivity.java
+++ app/src/main/java/com/cosc417/nelsm/shuffled/MainActivity.java
@@ -1,11 +1,20 @@
 package com.cosc417.nelsm.shuffled;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> dictionary;
+    private int minLength;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +28,18 @@ public class MainActivity extends AppCompatActivity {
 
         // this comment is to test commit
 
+    }
+
+    public void makeDictionary() {
+        dictionary = new ArrayList<String>();
+
+        BufferedReader dictionaryIn = null;
+        AssetManager am = this.getAssets();
+
+        try {
+            dictionaryIn = new BufferedReader(new InputStreamReader(am.open("words_alpha.txt")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
